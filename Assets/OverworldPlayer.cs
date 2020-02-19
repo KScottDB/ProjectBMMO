825fbf397ac632f7ab8d76c8a419fd02e4ab5721
KScott <kscott.db@gmail.com>
2020-02-19 04:32:57 -0800

    upgrade to new unity version, add character movement

--- Assets/OverworldPlayer.cs
+++ Assets/OverworldPlayer.cs
@@ -7,12 +7,20 @@ public class OverworldPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // setup camera
+        Camera.main.orthographicSize = Screen.height / 2;
     }
+
+    // PHYSICS
+    const float movespeed = 1f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Move character around
+        transform.position = new Vector3(
+            transform.position.x + (Input.GetAxis("Horizontal") * movespeed),
+            transform.position.y + (Input.GetAxis("Vertical")   * movespeed)
+        );
     }
 }
