7517222b00f0ffa422085ec1b6e214872ba69c8a
KScott <kscott.db@gmail.com>
2020-02-20 01:52:55 -0800

    add toad sprite and animations
    TODO: finish animations that aren't of direction D.

--- Assets/OverworldPlayer.cs
+++ Assets/OverworldPlayer.cs
@@ -4,23 +4,75 @@ using UnityEngine;
 
 public class OverworldPlayer : MonoBehaviour
 {
+    public string pclass = "toad";
+    public bool remote = false;
+
+    const int PPU = 32; // pixels per unit
+
+    float hspeed;
+    float vspeed;
+    char direction = 'D';
+    char state = 'W';
+
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        // setup camera
-        Camera.main.orthographicSize = Screen.height / 2;
+        animator = GetComponentInParent<Animator>();
+    }
+
+    char getDirection(float hspeed, float vspeed) {
+        const float deadzone = 0.05f;
+
+        bool nh = hspeed < deadzone; // Negative Hspeed
+        bool ph = hspeed > deadzone; // Positive Hspeed
+        bool nv = vspeed < deadzone; // Negative Vspeed
+        bool pv = vspeed > deadzone; // Positive Vspeed
+
+        char o = 'x'; // 'x' => unchanged
+
+        if (nh) o = 'A'; // Left
+        if (ph) o = 'B'; // Right
+        if (nv) o = 'C'; // Up
+        if (pv) o = 'D'; // Down
+
+        if (nh && pv) o = 'E'; // Northwest
+        if (nh && nv) o = 'F'; // Southwest
+        if (ph && pv) o = 'G'; // Northeast
+        if (ph && nv) o = 'H'; // Southeast
+
+        return o;
     }
 
     // PHYSICS
-    const float movespeed = 1f;
+    const float movespeed = 1f / PPU;
+                         // (1px / 32 PPU) per frame
 
     // Update is called once per frame
     void Update()
     {
         // Move character around
-        transform.position = new Vector3(
-            transform.position.x + (Input.GetAxis("Horizontal") * movespeed),
-            transform.position.y + (Input.GetAxis("Vertical")   * movespeed)
+        hspeed = Input.GetAxis("Horizontal") * movespeed;
+        vspeed = Input.GetAxis("Vertical") * movespeed;
+
+        // Animation code
+        if (hspeed < 0) direction = 'A'; // LEFT
+        if (hspeed > 0) direction = 'B'; // RIGHT
+        if (hspeed > 0) direction = 'C'; // FORWARD
+        if (vspeed < 0) direction = 'D'; // BACK
+
+        animator.speed = Mathf.Abs((hspeed + vspeed) * PPU);
+
+        animator.Play(pclass + state + direction);
+        
+        // if not moving, set to frame 0
+        if (animator.speed == 0)
+            animator.Play(0, -1, 0);
+
+        // Apply hspeed/vspeed
+        transform.position += new Vector3(
+            hspeed, vspeed
         );
     }
 }
---
+++ Assets/SnapToPixelGrid.cs
@@ -0,0 +1,20 @@
+﻿// mostly https://nielson.dev/2015/08/the-pixel-grid-better-2d-in-unity-part-1
+
+using UnityEngine;
+
+public class SnapToPixelGrid : MonoBehaviour {
+    [SerializeField]
+    private int pixelsPerUnit = 32;
+
+    /// <summary>
+    /// Snap the object to the pixel grid determined by the given pixelsPerUnit.
+    /// Using the parent's world position, this moves to the nearest pixel grid location by 
+    /// offseting this GameObject by the difference between the parent position and pixel grid.
+    /// </summary>
+    private void LateUpdate() {
+        transform.position = new Vector3(
+            Mathf.Round(transform.position.x * pixelsPerUnit) / pixelsPerUnit,
+            Mathf.Round(transform.position.y * pixelsPerUnit) / pixelsPerUnit
+        );
+    }
+}
